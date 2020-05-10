3c06cbc00847b5c341e882d6560d8e817b88148e
nojaja <free.riccia@gmail.com>
2020-05-10 14:56:51 +0000

    index.js更新 PublishFileContainerを利用可能にした

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { FileData } from './FileData.js'
 export { FileContainer } from './FileContainer.js'
 export { EditorFileData } from './EditorFileData.js'
+export { PublishFileContainer } from './PublishFileContainer.js'
